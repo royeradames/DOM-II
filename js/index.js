00e1ef8c2af6479cbfb28841093fecdccb176003
royeradames <royeraadames@gmail.com>
2020-07-01 09:36:06 -0400

    added click, and double click reizing for h2, and working on adding a pop out message when page is loadded

--- js/index.js
+++ js/index.js
@@ -18,3 +18,24 @@ Using the 10 unique events, find ways to update the DOM in creative ways. For ex
  Stop the navigation items from refreshing the page by using preventDefault()
 
 */
+const bodyH2 = document.querySelectorAll('h2');
+const bodyH4 = document.querySelectorAll('h4');
+
+//click on the h2 to double their sizes
+bodyH2.forEach(item => {
+    item.addEventListener('click', (event) =>{
+        event.target.style.fontSize = '5.2rem';
+    })
+})
+
+// normal size when h2 are double click
+bodyH2.forEach(item => {
+    item.addEventListener('dblclick', (event) =>{
+        event.target.style.fontSize = '3.2rem';
+    })
+})
+
+//add a message saying that the page is loaded
+window.addEventListener('load', (event) => {
+    alar('page is fully loaded');
+  });
