a90ec9078176bacd4f482a6b89836ad60c06b4e5
royeradames <royeraadames@gmail.com>
2020-07-07 18:39:56 -0400

    finish the 2 tasks of the tutorial, and now working on its 3. Making ircles to animate at the moment

--- stretch-assignment copy/index.js
+++ stretch-assignment copy/index.js
@@ -1,2 +1,18 @@
-gsap.to('.logo', {duration:2, x: 300});//1.50
+//first stask
+
+// gsap.to('.logo, .astronaut', {duration:2, x: 900, backgroundColor: '#560563', borderRadius: '20%', border: '5px solid white', ease: 'slow'});//1.50
+
+
+// //second task
+// gsap.set(`.logo, .astronaut`, {transformOrigin: `50%, 50%`});
+
+// gsap.to(`.logo, .astronaut`, {duration: 20, rotation: 360})
+
+// let myObject = {rotation: 0};
+// gsap.to(myObject, {duration:20, rotation: 360, onUpdate: function(){
+//     console.log(myObject.rotation)
+// }})
+
+
+//3 task
 
